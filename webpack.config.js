ce467878ad1441ca4f63cefcd921f7584d73c948
Asilbek Asilov <asil.california@gmail.com>
2024-09-14 00:35:23 -0700

    Add funtion to generate jokes

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: '[name][contenthash].js',
         clean: true,
+        assetModuleFilename: '[name][ext]'
 	},
     devtool: 'source-map',
 	devServer: {
@@ -38,7 +39,11 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     },
                 }
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
 		],
 	},
 	plugins: [
